f2c3fe6682958aaf28fb1bde41bb189166801f5d
saikumar <saikumarlaveti5061@gmail.com>
2025-03-03 21:19:43 +0530

    start javascript

---
+++ first.js
@@ -0,0 +1,26 @@
+// document.write("Good Evening ,Saikumar  ")
+// RollNo = 40
+// Name   = "  Saikumar Laveti"
+// document.write(RollNo)
+// document.write(Name)
+
+// const a = 40
+// console.log(a)
+
+{
+    var a = 40
+    console.log(a)
+}
+console.log(a)
+
+{
+    let b = 30
+    console.log(b)
+}
+//console.log(b)
+
+{
+    const c = 20
+    console.log(c)
+}
+//console.log(c)
---
+++ function.js
@@ -0,0 +1,11 @@
+var Fan = {
+    "Name" : "Usha",
+    "Price" : 2500,
+}
+
+// document.writeln(Fan);
+for(var k in Fan){
+    // document.writeln(k);
+    document.writeln(Fan)
+    document.writeln(Fan[k]);
+}
